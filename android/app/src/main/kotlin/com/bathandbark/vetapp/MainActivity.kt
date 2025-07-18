90b7b942b42e8ae2b480889863c890d86fb19242
Comi <meekou19@gmail.com>
2025-07-18 17:08:58 +0800

    not fixed google sign in

---
+++ android/app/src/main/kotlin/com/bathandbark/vetapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bathandbark.vetapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/example/paws/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.paws
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
